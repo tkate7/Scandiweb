47c8f51119865b2e8d2fea6143914e1a58031a7d
Kate Tampus <121474607+tkate7@users.noreply.github.com>
2023-02-09 00:44:51 -0600

    Update Add_product_page.php
    added favicon link

--- View/Main/Add_product_page.php
+++ View/Main/Add_product_page.php
@@ -2,8 +2,8 @@
     <head>
         <meta charset="utf-8">
         <title>Add Products</title>
-        <link rel="stylesheet" type="text/css" href="../../Public/CSS/Styles.css">
-        <link rel="icon" type="image/x-icon" href="">
+        <link rel="stylesheet" type="text/css" href="../../Public/CSS/Style.css">
+        <link rel="icon" type="image/x-icon" href="../../Public/favicon.png">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js"></script>
         <h2>Add Products</h2>
     </head>
